75be42977bb407a5d085ae6d9600573e8f423426
wsong2016 <song@weis-macbook.local>
2016-04-08 10:42:41 +0800

    Create a new iOS app project for teaching

--- Hello-Shou/ViewController.swift
+++ Hello-Shou/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var shouLogo: UIImageView!
+    
+    var showIfTheShouLogoAppear:Bool = true
+    
+    @IBAction func clickMe(sender: UIButton) {
+        if showIfTheShouLogoAppear{
+            shouLogo.hidden = true
+            showIfTheShouLogoAppear = false
+        }else{
+            shouLogo.hidden = false
+            showIfTheShouLogoAppear = true
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
